6851df5606808cc3f641460a4f4e6ad27b20ef2f
Shawn Cavazos <scavazos4@gmail.com>
2021-06-21 17:47:02 -0500

    note

--- app.py
+++ app.py
@@ -2,27 +2,20 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-# Create an instance of Flask
-app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
+app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Route to render index.html template using data from Mongo
 @app.route("/")
 def index():
     mars_data = mongo.db.mars.find_one()
     print(mars_data)
     return render_template("index.html", indexlink=mars_data)
 
-# Route that will trigger the scrape function
 @app.route("/scrape")
-def scrape_all():
-    mars_scrape = scrape_mars.scrape_all()
+def scrape():
+    mars_scrape = scrape_mars.scrape()
     mongo.db.mars.update({}, mars_scrape, upsert=True)
-    
-    
-    # Redirect to home page
     return redirect("/")
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,5 +1,3 @@
-# Seting up dependacies for MongoDB and Flask application
-
 from splinter import Browser
 from bs4 import BeautifulSoup as bs
 import pandas as pd
@@ -13,7 +11,7 @@ def init_browser():
 def scrape_all():
     browser = init_browser()
 
-    # NASA Mars News define and retrive
+
     url = "https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
     response = requests.get(url)
     soup = bs(response.text, 'html.parser')
@@ -43,29 +41,21 @@ def scrape_all():
 
 
     # Mars Facts
-    # Have pandas read any tables on mars facts page
     url2= 'https://space-facts.com/mars/'
     table = pd.read_html(url2)
-    # Filter to table I want to work with
     factsTable = table[0]
     factsTable_df = pd.DataFrame(factsTable)
     factsTable_df = factsTable_df.rename({0:"Describe" , 1:"Mars"}, axis =1)
-    # Convert to html string & clean
     factsTableHtml = factsTable_df.to_html()
 
     # Mars Hemispheres
-    # Set up for retriving browser
     url3 = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url3)
     html = browser.html
     soup = bs(html, "html.parser")
-    # Setting up bucket
     hemisImages = []
-    # Base image url
     url4 = "https://astrogeology.usgs.gov/"
-    # Set up soup
     hemis = bs.find_all('div', class_='item')
-    # Setting up loop to get title and url
     for hemi in hemis:
         title = hemi.find('h3').text
     
@@ -77,15 +67,9 @@ def scrape_all():
         images_url = url4+imgsUrl
         hemisImages.append({"title": title, "img_url": images_url})
     
-
-
-
-
-
-    # Close browser after scraping
     browser.quit()
     
-    # Creating dictionary for all of the scrapped info
+
     mars={
         "Mars_news_headlines": title,
         "Mars_News_Teasers": teaser,
@@ -93,9 +77,8 @@ def scrape_all():
         "Mars_Facts": factsTableHtml,
         "Mars_Hemispheres": hemisImages,
         }
-    # mars_dic = "Hello World"
     return mars
 
 if __name__ == "__main__":
-    data = scrape_all()
+    data = scrape()
     print(data)
